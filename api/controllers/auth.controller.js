134cf3554210ca767c706e268b7715e72ca7cf47
yusufinann <yusuf.inan2@ogr.sakarya.edu.tr>
2024-08-19 23:19:16 +0300

    İnstalled json web token.Completed backend part of login operation.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs"
-
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const register = async (req, res) => {
   try {
@@ -36,3 +36,29 @@ export const register = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+export const login = async (req, res) => {
+  try {
+     const {email,password}=req.body;
+  
+     const user = await User.findOne({ email });
+     const isPasswordCorrect=await bcrypt.compare(password,user?.password || "");
+  
+     if(!user ||!isPasswordCorrect)
+     {
+      return res.status(400).json({ error: "Invalid username or password" });
+     }
+     await generateTokenAndSetCookie(user._id,res);
+  
+     res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+     })
+  
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ error: "internal server error" });
+  }
+  };
+  
--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { register } from "../controllers/auth.controller.js";
+import { register,login } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 // Kullanıcı kaydı için bir POST isteği
 router.post("/register", register);
+router.post("/login", login);
 
 
 export default router;
